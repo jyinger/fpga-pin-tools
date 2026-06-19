b683e7228de13f8c97664b1ddc081f35cc83c756
Jack Yinger <jyinger@cytekbio.com>
2026-06-19 14:08:27 -0700

    Starts of better function fro pinout_csv_updater, refactored existing fucntion to be cleaner.

--- pinout_csv_updater.py
+++ pinout_csv_updater.py
@@ -10,36 +10,6 @@ parser.add_argument('-altium', help='Altium pin CSV, source of update info.')
 parser.add_argument('-o', help='Output Vivado CSV file.')
 args = parser.parse_args()
 
-# FPGA CSV Column Names
-#'IO Bank'
-fc_pin = 'Pin Number'
-#'Site'
-#'Site Type'
-#'Min Trace Delay (ps)'
-#'Max Trace Delay (ps)'
-#'Prohibit'
-#'Interface'
-fc_net = 'Signal Name'
-#'Direction'
-#'DiffPair Type'
-fc_diffpair = 'DiffPair Signal'
-#'IO Standard'
-#'Drive (mA)'
-#'Slew Rate'
-#'OUTPUT_IMPEDANCE'
-#'PRE_EMPHASIS'
-#'LVDS_PRE_EMPHASIS'
-#'OFFSET_CONTROL'
-#'EQUALIZATION'
-#'Pull Type'
-#'DQS_BIAS'
-#'IN_TERM'
-#'DIFF_TERM'
-#'OFFCHIP_TERM'
-#'Board Signal'
-#'Board Voltage'
-
-
 def site_is_io(site):
     return site.startswith("IO_")
 
@@ -65,6 +35,75 @@ with open(args.altium) as f_in:
 #    print((k, v))
 
 
+# FPGA CSV Column Names
+#'IO Bank'
+fc_pin = 'Pin Number'
+#'Site'
+fc_site = 'Site Type'
+#'Min Trace Delay (ps)'
+#'Max Trace Delay (ps)'
+#'Prohibit'
+#'Interface'
+fc_net = 'Signal Name'
+#'Direction'
+#'DiffPair Type'
+fc_diffpair = 'DiffPair Signal'
+#'IO Standard'
+#'Drive (mA)'
+#'Slew Rate'
+#'OUTPUT_IMPEDANCE'
+#'PRE_EMPHASIS'
+#'LVDS_PRE_EMPHASIS'
+#'OFFSET_CONTROL'
+#'EQUALIZATION'
+#'Pull Type'
+#'DQS_BIAS'
+#'IN_TERM'
+#'DIFF_TERM'
+#'OFFCHIP_TERM'
+#'Board Signal'
+#'Board Voltage'
+
+
+csv_in_rows = []
+with open(args.vivado) as f_in:
+    csv_in = csv.reader(f_in)
+
+    # Find the header
+    header_found = False
+    while not header_found:
+        header = csv_in.__next__()
+        if len(header) > 0 and header[0] == 'IO Bank':
+            header_found = True
+
+    header_asoc = {}
+    for i in range(len(header)):
+        header_asoc[header[i]] = i
+
+    # Capture pinout rows
+    for r in csv_in:
+        csv_in_rows.append(r)
+
+
+def io_details(row):
+    return row[header_asoc[fc_net]:]
+
+
+net_std_map = {}
+for r in csv_in_rows:
+    net = r[header_asoc[fc_net]]
+    if net != '':
+        net_std_map[net] = io_details(r)
+    else:
+        if site_is_io(r[header_asoc[fc_site]]):
+            print(r)
+            print(io_details(r))
+            print('')
+
+#for k, v in net_std_map.items():
+#    print((k, v))
+
+
 def other_pair_member(net):
     if '_n' in net:
         return net.replace('_n', '_p')
@@ -72,52 +111,38 @@ def other_pair_member(net):
         return net.replace('_p', '_n')
 
 
-with open(args.vivado) as f_in:
-    with open(args.o, 'w') as f_out:
-        csv_in = csv.reader(f_in)
-        csv_out = csv.writer(f_out)
+# TODO UESE ME: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
 
-        # New header:
-        csv_out.writerow(['# Updated by pinout_csv_updater.py'])
-        csv_out.writerow(['#    Input Vivado Pinout: {}'.format(args.vivado)])
-        csv_out.writerow(['#    Altium Pinout with Updates: {}'.format(args.altium)]) 
-        csv_out.writerow(['#    Output Vivado Pinout: {}'.format(args.o)])
+with open(args.o, 'w') as f_out:
+    csv_out = csv.writer(f_out)
 
-        # Find the header
-        header_found = False
-        while not header_found:
-            header = csv_in.__next__()
-            if len(header) > 0 and header[0] == 'IO Bank':
-                header_found = True
+    # New header:
+    csv_out.writerow(['# Updated by pinout_csv_updater.py'])
+    csv_out.writerow(['#    Input Vivado Pinout: {}'.format(args.vivado)])
+    csv_out.writerow(['#    Altium Pinout with Updates: {}'.format(args.altium)])
+    csv_out.writerow(['#    Output Vivado Pinout: {}'.format(args.o)])
+    csv_out.writerow(header)
 
-        csv_out.writerow(header)
-        header_asoc = {}
-        for i in range(len(header)):
-            header_asoc[header[i]] = i
+    for r in csv_in_rows:
 
-        for r in csv_in:
+        pin = r[header_asoc[fc_pin]]
 
-            pin = r[header_asoc[fc_pin]]
 
-            if pin in pin_net_map:
-                net = r[header_asoc[fc_net]]
-                new_net = pin_net_map[pin]
-                if net != new_net:
-                    print("Change: Swap or Add")
-                    print(r)
-                    r[header_asoc[fc_net]] = new_net
-                    r[header_asoc[fc_diffpair]] = other_pair_member(new_net)
-                    print(r)
-                    print('')
-            else:
-                net = r[header_asoc[fc_net]]
-                new_net = ''
-                if net != new_net:
-                    print("Change: Remove")
-                    print(r)
-                    r[header_asoc[fc_net]] = new_net
-                    r[header_asoc[fc_diffpair]] = other_pair_member(new_net)
-                    print(r)
-                    print('')
-            
-            csv_out.writerow(r)
+        net = r[header_asoc[fc_net]]
+        if pin in pin_net_map:
+            new_net = pin_net_map[pin]
+            if net != new_net:
+                print("Change: Swap or Add")
+        else:
+            new_net = ''
+            if net != new_net:
+                print("Change: Remove")
+
+        if net != new_net:
+            print(r)
+            r[header_asoc[fc_net]] = new_net
+            r[header_asoc[fc_diffpair]] = other_pair_member(new_net)
+            print(r)
+            print('')
+
+        csv_out.writerow(r)
